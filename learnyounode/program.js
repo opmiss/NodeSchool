5f9d1f1e16b8c87a6a4eec618a890d952bcac278
opmiss <opmiss@gmail.com>
2015-04-12 16:26:26 -0700

    node fs

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,4 +1,20 @@
 var arr = process.argv;
-var fs = require('fs'); 
-var bf = fs.readFileSync(arr[2]);
-console.log(bf.toString().split('\n').length-1); 
+var fs = require('fs');
+var dir = arr[2]; 
+var type = null; 
+if (arr.length>3) type = arr[3];  
+/*fs.readFile(arr[2], function(err, data){
+    if (err) throw err; 
+    console.log(data.toString().split('\n').length-1); 
+});*/
+fs.readdir(dir, function(err, files){
+    if (err) throw err;  
+    if (type){
+	files = files.filter(function(e){
+	    if (e.substring(e.length-type.length-1)===('.'+type))
+		return e;     
+	    }); 
+    }
+    for (var i in files)
+	console.log(files[i]); 
+}); 
