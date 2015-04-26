125d8aec3c43edbccf45c2f97cfc3af615517c59
opmiss <opmiss@gmail.com>
2015-04-25 22:38:05 -0700

    html stream

--- stream-adventure/html_stream.js
+++ stream-adventure/html_stream.js
@@ -1,6 +1,16 @@
 var trumpet = require('trumpet');
 var fs = require('fs');
 var tr = trumpet(); 
-fs.createReadStream('input.html').pipe(tr);
-var stream = tr.select('.beep').createStream(); 
 
+var through = require('through2');
+function write(buffer, encoding, next){
+    this.push(buffer.toString().toUpperCase());
+    next(); 
+}
+function end(done){
+    done(); 
+}
+var transform = through(write, end);
+var loud = tr.select('.loud').createStream();
+loud.pipe(transform).pipe(loud); 
+process.stdin.pipe(tr).pipe(process.stdout); 
