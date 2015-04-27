5455814761ad006144995ee8c9509d4e56caebaf
opmiss <opmiss@gmail.com>
2015-04-26 23:46:58 -0700

    duplex

--- stream-adventure/duplexer_redux.js
+++ stream-adventure/duplexer_redux.js
@@ -1,2 +1,15 @@
+var duplexer = require('duplexer2');
+var through = require('through2').obj;  
 module.exports = function(counter){
+    var counts={}; 
+    function write(d, _, next){
+	counts[d.country] = (counts[d.country]||0)+1; 
+	next(); 
+    }
+    function end(done){
+	counter.setCounts(counts); 
+	done(); 
+    }
+    var input = through(write, end);
+    return duplexer(input, counter); 
 }
--- stream-adventure/lines.js
+++ stream-adventure/lines.js
@@ -1,8 +1,8 @@
 var through2 = require('through2'); 
 var split = require('split');
-var count =0; 
+var count = 0; 
 process.stdin.pipe(split())
-.pipe(through2(function (line, _, next) {
+.pipe(through2(function(line, _, next) {
     if (count%2==0)
 	console.log(line.toString().toLowerCase());
     else
