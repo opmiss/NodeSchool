88a6244507641f7b1b5efc40d66a2abdcac20d21
opmiss <opmiss@gmail.com>
2015-11-08 13:14:44 -0800

    props

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -15,11 +15,30 @@ var TodoList = React.createClass({
 	render: function(){
 		return (
 		  <div className="todoList">
-		    I am a TodoList. 
+		    <table style={{border: "2px solid black"}}>
+			  <tbody>
+			    <Todo title="Shopping">Milk</Todo>
+				<Todo title="Hair cut">13:00</Todo>
+			  </tbody>
+		    </table>
 		  </div>
 		); 
 	}
 });
+
+var Todo = React.createClass({
+	propTypes: {
+		title: React.PropTypes.string.isRequired
+	}, 
+	render: function(){
+		return (
+		  <tr>
+		     <td style={{border: "1px solid black"}}>{this.props.title}</td>
+			 <td style={{border: "1px solid black"}}>{this.props.children}</td>
+		  </tr>
+		); 
+	}
+}); 
 
 var TodoForm = React.createClass({
 	render: function(){
